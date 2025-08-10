0890a40ef2ec7c5677e9ef709ab38f3df0fc6877
riadkabir45 <riadkabir45@gmail.com>
2025-08-10 11:19:38 +0600

    feat: symbol_info and scope table

--- L2/scope_table.h
+++ L2/scope_table.h
@@ -11,6 +11,12 @@ private:
     int hash_function(string name)
     {
         // write your hash function here
+        int hash_value = 0;
+        for (char c : name)
+        {
+            hash_value += ((int)c - (int)'a');
+        }
+        return hash_value % bucket_count;
     }
 
 public:
@@ -22,17 +28,101 @@ public:
     bool insert_in_scope(symbol_info* symbol);
     bool delete_from_scope(symbol_info* symbol);
     void print_scope_table(ofstream& outlog);
-    ~scope_table();
+    //~scope_table();
 
     // you can add more methods if you need
 };
 
+scope_table::scope_table(int bucket_count, int unique_id, scope_table *parent_scope)
+{
+    this->bucket_count = bucket_count;
+    this->unique_id = unique_id;
+    this->parent_scope = parent_scope;
+    table.resize(bucket_count);
+}
 
+
+scope_table::scope_table() : scope_table(10, 0, NULL) { }
+
+int scope_table::get_unique_id()
+{
+    return unique_id;
+}
+
+scope_table *scope_table::get_parent_scope()
+{
+    return parent_scope;
+}
+
+bool scope_table::insert_in_scope(symbol_info* symbol)
+{
+    int index = hash_function(symbol->get_name());
+    table[index].push_back(symbol);
+    return true;
+}
+
+bool scope_table::delete_from_scope(symbol_info* symbol)
+{
+    int index = hash_function(symbol->get_name());
+    for (auto it = table[index].begin(); it != table[index].end(); ++it)
+    {
+        if ((*it)->get_name() == symbol->get_name())
+        {
+            table[index].erase(it);
+            return true;
+        }
+    }
+    return false;
+}
 
 // complete the methods of scope_table class
 void scope_table::print_scope_table(ofstream& outlog)
 {
     outlog << "ScopeTable # "+ to_string(unique_id) << endl;
+
+    int counter = 0;
+    for (auto bucket_it = table.begin(); bucket_it != table.end(); ++bucket_it)
+    {
+        if (!bucket_it->empty())
+        {
+            for (auto symbol : *bucket_it)
+            {
+                outlog << to_string(counter++) << " --> " << endl;
+                outlog << "< " << symbol->get_name() << " : ID >" << endl;
+
+                if(symbol->is_variable())
+                    outlog << "Variable" << endl;
+                else if(symbol->is_array())
+                    outlog << "Array" << endl;
+                else if(symbol->is_function())
+                    outlog << "Function Definition" << endl;
+
+
+                if(symbol->is_function())
+                {
+                    outlog << "Return Type: " << symbol->get_type() << endl;
+                    outlog << "Number of Parameters: " << symbol->get_parameters().size() << endl;
+                    outlog << "Parameter Details: ";
+                    bool first_param = true;
+                    for(auto param : symbol->get_parameters())
+                    {
+                        if(first_param){
+                            outlog << param.get_type() << " " << param.get_name();
+                            first_param = false;
+                        }
+                        else
+                            outlog << ", " << param.get_type() << " " << param.get_name();
+                    }
+                    outlog << endl;
+                }else{
+                    outlog << "Type: " << symbol->get_type() << endl;
+                }
+                if(symbol->is_array())
+                    outlog << "Size: " << symbol->get_array_size() << endl;
+            }
+            outlog << endl;
+        }
+    }
 
     //iterate through the current scope table and print the symbols and all relevant information
 }
--- L2/symbol_info.h
+++ L2/symbol_info.h
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+enum class symbol_type
+{
+    VARIABLE,
+    ARRAY,
+    FUNCTION
+};
+
 class symbol_info
 {
 private:
@@ -8,36 +15,97 @@ private:
     string type;
 
     // Write necessary attributes to store what type of symbol it is (variable/array/function)
+    symbol_type sym_type;
     // Write necessary attributes to store the type/return type of the symbol (int/float/void/...)
     // Write necessary attributes to store the parameters of a function
+    vector<symbol_info> parameters;
     // Write necessary attributes to store the array size if the symbol is an array
+    int array_size = -1;
 
 public:
-    symbol_info(string name, string type)
+    symbol_info(string name, string type, symbol_type sym_type = symbol_type::VARIABLE, vector<symbol_info> parameters = {}, int array_size = -1)
     {
         this->name = name;
         this->type = type;
+        this->sym_type = sym_type;
+        if (sym_type == symbol_type::FUNCTION)
+        {
+            this->parameters = parameters;
+        }else if (sym_type == symbol_type::ARRAY)
+        {
+            this->array_size = array_size;
+        }
     }
+
     string get_name()
     {
         return name;
     }
+
     string get_type()
     {
         return type;
     }
+
     void set_name(string name)
     {
         this->name = name;
     }
+
     void set_type(string type)
     {
         this->type = type;
     }
+
     // Write necessary functions to set and get the attributes
+    symbol_type get_symbol_type()
+    {
+        return sym_type;
+    }
+
+    void set_symbol_type(symbol_type type)
+    {
+        this->sym_type = type;
+    }
+
+    vector<symbol_info> get_parameters()
+    {
+        return parameters;
+    }
+
+    void set_parameters(vector<symbol_info> params)
+    {
+        this->parameters = params;
+    }
+
+    int get_array_size()
+    {
+        return array_size;
+    }
+
+    void set_array_size(int size)
+    {
+        this->array_size = size;
+    }
+
+    bool is_variable()
+    {
+        return sym_type == symbol_type::VARIABLE;
+    }
+
+    bool is_array()
+    {
+        return sym_type == symbol_type::ARRAY;
+    }
+
+    bool is_function()
+    {
+        return sym_type == symbol_type::FUNCTION;
+    }
 
     ~symbol_info()
     {
         // Write necessary code to deallocate memory, if necessary
+        parameters.clear();
     }
 };
